ab222072048d5e5d4e4fbe947b68f48d1a6563f2
yutanohara <ynohara04@gmail.com>
2018-03-14 16:43:22 +0900

    イベント機能追加

--- e-assistant/app/models/user.rb
+++ e-assistant/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :user_id, presence: true, uniqueness: true
+  validates :user_id, {presence: true, uniqueness: true}
   validates :password, {presence: true, confirmation: true,
                         length: { in: 4..15}}
   validates :email, uniqueness: true
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
