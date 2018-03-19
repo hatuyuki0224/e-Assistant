8c0c26fd2b7e70900a49500b4929aaa91c840180
岡田悠希 <okadayuki@okadayukinoMacBook-ea.local>
2018-03-19 23:16:19 +0900

    [fix] events#new時にパラメータが無いとエラーになるのを修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -82,6 +82,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :start, :end, :color, :allDay)
+      params.fetch(:event, {}).permit(:title, :start, :end, :color, :allDay)
     end
 end
