bb9698f363226bbd7323f9a715f52c260f7e67e8
Yuki Okada <nemu.hatuyuki0224@gmail.com>
2018-03-28 03:22:37 +0900

    [update] メソッド名を変更
    set_current_user => current_user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user, :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user
 
   # GET /events
   # GET /events.json
   def index
-    @events = set_current_user.events.all
+    @events = current_user.events.all
   end
 
   # GET /events/1
@@ -14,7 +15,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = set_current_user.events.build(event_params)
+    @event = current_user.events.build(event_params)
     respond_to do |format|
       format.html
       format.js
@@ -32,7 +33,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = set_current_user.events.build(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -78,7 +79,7 @@ class EventsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
-      @event = set_current_user.events.find(params[:id])
+      @event = current_user.events.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
